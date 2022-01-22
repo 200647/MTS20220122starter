d62bc0b2c6c04b9d0376750ac69a725559953330
200647 <C200647@ccg.ac.jp>
2022-01-22 10:12:23 +0900

    mtssoft

---
+++ src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -0,0 +1,55 @@
+package com.example.demo.app.inquiry;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/*
+ * Add annotations here
+ */
+public class InquiryController {
+
+// 	private final InquiryService inquiryService;
+
+
+
+	@GetMapping
+	public String index(Model model) {
+
+		//hands-on
+
+		return "inquiry/index";
+	}
+
+	@GetMapping("/form")
+	public String form(/*Add parameters. */) {
+
+		return "inquiry/form";
+	}
+
+	@PostMapping("/form")
+	public String formGoBack(InquiryForm inquiryForm, Model model) {
+		model.addAttribute("title", "InquiryForm");
+		return "inquiry/form";
+	}
+
+
+	@PostMapping("/confirm")
+	public String confirm(/*Add parameters. */) {
+
+		//hands-on
+
+		return "inquiry/confirm";
+	}
+
+	@PostMapping("/complete")
+	public String complete(/*Add parameters. */) {
+
+		//hands-on
+
+		//redirect
+
+		return "";
+	}
+
+}
